219b2f3a9db673a438247ed2fc72d66f95a49e64
shabnam <shabnamjm@code.edu.az>
2024-11-19 23:10:22 +0400

    server side some changes added

---
+++ server/index.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import cors from "cors";
+
+import authRoutes from "./routes/auth";
+import usersRoutes from "./routes/users";
+import "./mongoose/schemas/user";
+import "./auth/local-strategy";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+        },
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+async function connecToDb() {
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.myz60.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+}
+
+connecToDb()
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.log(err));
---
+++ server/routes/auth.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+import {
+    forgotPasswordSchema,
+    registerSchema,
+    resetPasswordSchema,
+} from "../validation/auth";
+
+import { authenticate } from "../middlewares/user";
+
+import validateSchema from "../middlewares/validation";
+import authController from "../controllers/auth";
+
+const router = Router();
+
+router.post("/login", authenticate, authController.login);
+
+router.post("/register", validateSchema(registerSchema), authController.register);
+
+router.post("/logout", authController.logout);
+
+router.get("/current-user", authController.currentUser);
+
+router.post(
+    "/forgot-password",
+    validateSchema(forgotPasswordSchema),
+    authController.forgotPassword
+);
+
+router.post(
+    "/reset-password",
+    validateSchema(resetPasswordSchema),
+    authController.resetPassword
+);
+export default router;
